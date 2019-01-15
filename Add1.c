9d6dcbb1e598d128459ba257e18467fa51739423
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-01-15 11:06:29 +0530

    Add files via upload

---
+++ Add1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int input()
+{
+int a;
+printf("Enter a number\n");
+scanf("%d",&a);
+return a;
+}
+int compute(int b)
+{
+b=b+1;
+return b;
+}
+void output(int x)
+{
+printf("after adding 1 the number is %d\n",x);
+}
+int main()
+{
+int y;
+y=input();
+int z=compute(y);
+output(z);
+return 0;
+}
+
+
+
