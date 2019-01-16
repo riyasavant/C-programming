74ce2fc16bf0d6df03a71d010928479fbc23a4c1
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-01-16 17:25:10 +0530

    Add files via upload

---
+++ add2error.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void input(int *x,int *y)
+{
+  printf("Enter two numbers\n");
+  scanf("%d %d",x,y);
+ }
+int compute(int a,int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+void output(int k)
+{
+  printf("after adding the numbers the output is %d\n",k);
+}
+int main()
+{
+  int r,s;
+  input(r,s);
+  int z=compute(r,s);
+  output(z);
+  return 0;
