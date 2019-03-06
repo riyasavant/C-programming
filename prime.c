bad27e3a12481df99349a1c393066d72f947ce58
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-03-06 17:36:35 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int input()
+{
+ int n;
+ printf("Enter the number of prime numbers to be generated\n");
+ scanf("%d",&n);
+ return n;
+}
+
+void compute(int n,int a[])
+{
+  int i=2,x=0;
+ while(n>0)
+ {
+  if(i%2==0||i%3==0||i%5==0)
+  {
+   i++;
+  }
+  else
+  {
+   a[x]=i;
+   i++;
+   n--;
+   x++;
+  }
+  
+ } 
+}
+void output(int b[],int n)
+{
+ for(int i=0;i<n;i++)
+ {
+  printf("%d\n",b[i]);
+ }
+}
+int main()
+{
+ int x;
+ x=input();
+ int a[x];
+ compute(x,a);
+ output(a,x);
+ return 0;
+}
+
+
