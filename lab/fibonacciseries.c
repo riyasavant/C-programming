ee5c7e6f03d4cad008bd5989c63d1a00068968b8
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-03-08 18:38:52 +0530

    Update fibonacciseries.c

--- lab/fibonacciseries.c
+++ lab/fibonacciseries.c
@@ -1,39 +1,40 @@
 #include<stdio.h>
-void output(int l[],int m);
-int input()
+int input_range()
 {
  int n;
  printf("Enter the elements in the fibonacci series\n");
  scanf("%d",&n);
  return n;
 }
-void fibo(int x)
+void fibonacci(int x,int ar[])
 {
- int a=1,b=1,z,k[x-2],j=0;
- for(int i=2;i<x;i++)
+ int a=1,b=1,temp,j=0,i;
+ for(i=2;i<x;i++)
  {
-  z=a+b;
-  k[j]=z;
+  temp=a+b;
+  ar[j]=temp;
   j++;
   a=b;
-  b=z;
+  b=temp;
  }
- output(k,x);
 }
-void output(int l[],int m)
+void output(int a[],int x)
 {
+ int j;
  printf("The fibonacci series is\n");
  printf("1 1 ");
- for(int j=0;j<m-2;j++)
+ for(j=0;j<x-2;j++)
  {
-  printf("%d ",l[j]);
+  printf("%d ",a[j]);
  }
 }
 int main()
 {
- int c;
- c=input();
- fibo(c);
+ int n;
+ n=input_range();
+ int a[n-2];
+ fibonacci(n,a);
+ output(a,n);
  return 0;
 }
 
