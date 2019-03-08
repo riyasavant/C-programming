36dcadb6864644e2205159b750d52d5d412c8cdb
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-03-08 16:19:58 +0530

    Update ternary.c

--- ternary.c
+++ ternary.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
-void input(int *x,int *y, int *z)
+void input3num(int *x,int *y, int *z)
 {
   printf("Enter three numbers\n");
   scanf("%d %d %d",x,y,z);
  }
-int compute(int a,int b,int c)
+int smallest(int a,int b,int c)
 {
   int small;
   small=a<b?(a<c?a:c):(b<c?b:c);
   return small;
 }
-void output(int k)
+void result(int small)
 {
-  printf("The smaller of the two numbers is %d\n",k);
+  printf("The smaller of the three numbers is %d\n",small);
 }
 int main()
 {
-  int e,f,g;
-  input(&e,&f,&g);
-  int z=compute(e,f,g);
-  output(z);
+  int x1,x2,x3;
+  input(&x1,&x2,&x3);
+  int small=smallest(x1,x2,x3);
+  result(small);
   return 0;
 }
