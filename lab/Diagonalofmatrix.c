71dc0894447459c13b31cd65e26cc56ffc5d757c
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-03-08 17:30:49 +0530

    Update Diagonalofmatrix.c

--- lab/Diagonalofmatrix.c
+++ lab/Diagonalofmatrix.c
@@ -1,52 +1,56 @@
 #include<stdio.h>
-int input()
+int size_of_matrix()
 {
     int n;
     printf("Enter the size of the matrix\n");
     scanf("%d",&n);
     return n;
 }
-void compute(int n,int a[n][n],int ar[])
+void input_matrix(int n,int a[n][n])
 {
     printf("Enter the values of the matrix\n");
-    
-    for(int i=0;i<n;i++)
+    int i,j;
+    for(i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(j=0;j<n;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    int k=0;
-    for(int i=0;i<n;i++)
+}
+void diagonal(int n,int a[n][n],int arr[])
+{
+    int k=0,i,j;
+    for(i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(j=0;j<n;j++)
         {
             if(i==j)
             {
-                ar[k]=a[i][j];
+                arr[k]=a[i][j];
                 k++;
             }
         }
     }
-   
-    
-}
-void output(int a[],int n)
+}  
+
+void result(int arr[],int n)
 {
+    int i;
     printf("The diagonal elements are\n");
-    for(int i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
-        printf("%d\n",a[i]);
+        printf("%d\n",arr[i]);
     }
 }
 int main()
 {
     int n;
-    n=input();
+    n=size_of_matrix();
     int a[n][n];
-    int ar[n];
-    compute(n,a,ar);
-    output(ar,n);
+    int arr[n];
+    input_matrix(n,a);
+    diagonal(n,a,arr);
+    result(arr,n);
     return 0;
 }
