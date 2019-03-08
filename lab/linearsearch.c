3640ec038eb63a3f01cb26e9ec81b8bf34ade626
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-03-08 18:48:41 +0530

    Update linearsearch.c

--- lab/linearsearch.c
+++ lab/linearsearch.c
@@ -1,53 +1,51 @@
 #include<stdio.h>
-void input();
-void compute(int b[],int x,int y);
-void output(int x,int y);
-void output1(int x);
-void input()
+int array_size()
 {
-  int n,x;
-  printf("Enter the number of values in the array\n");
+  int n;
+  printf("Enter the size of the array\n");
   scanf("%d",&n);
-  int a[n];
+  return n;
+}
+void array(int a[],int n)
+{
+  int i;
   printf("Enter the values in the array\n");
-  for(int i=0;i<n;i++)
+  for(i=0;i<n;i++)
   {
     scanf("%d",&a[i]);
   }
+}
+int search()
+{
+  int x;
   printf("Enter the number to be searched\n");
   scanf("%d",&x);
-  compute(a,n,x);
+  return x;
 }
-void compute(int b[],int x,int y)
+void linearsearch(int a[],int x,int n,int *pos)
 {
-  int pos=0;
-  for(int i=0;i<x;i++)
+  int i;
+  for(i=0;i<n;i++)
   {
-    if(b[i]==y)
+    if(a[i]==x)
     {
-     pos=i+1;
+     *pos=i+1;
     }
   }
-  if(pos==0)
-  {
-   output1(y);
-  }
-  else
-  {
-   output(pos,y);
-  }
 }
-void output(int x,int y)
+void output(int x,int pos)
 {
- printf("The value %d is found at the position %d in the array\n",y,x);
-}
-void output1(int x)
-{
- printf("The value %d is not found in the array\n",x);
+ printf("The value %d is found at the position %d in the array\n",x,pos);
 }
 
 int main()
 {
-  input();
+  int n,x,pos;
+  n=array_size();
+  int a[n];
+  array(a,n);
+  x=search();
+  linearsearch(a,x,n,&pos);
+  output(x,pos);
   return 0;
 }
