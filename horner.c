5322ce61dd4b122988a501999397b94e8a4a683b
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-03-08 17:20:32 +0530

    Update horner.c

--- horner.c
+++ horner.c
@@ -1,41 +1,51 @@
 #include<stdio.h>
 #include<math.h>
-void input();
-void compute(float b[],int k, float l);
-void output(float o);
-
-void input()
+int power()
 {
-  int p;
-  float x;
-  printf("Enter the highest power of the polynomial\n");
-  scanf("%d",&p);
-  float a[p+1];
-  printf("Enter the valur of x\n");
-  scanf("%f",&x);
-  printf("Enterr the values of co-efficients of the polynomial       ax^0+bx^1..\n");
-  for(int i=0;i<=p;i++)
+ int p;
+ printf("Enter the highest power of the polynomial\n");
+ scanf("%d",&p);
+ return p;
+}
+int value_of_x()
+{
+ int x;
+ printf("Enter the value of x\n");
+ scanf("%d",&x);
+ return x;
+}
+void coefficients(float a[],int p)
+{
+  int i;
+  printf("Enter the values of co-efficients of the polynomial\n");
+  for(i=0;i<=p;i++)
   {
    scanf("%f",&a[i]);
   } 
-  compute(a,p,x);
 }
-void compute(float b[],int k, float l)
+int compute_poly(float a[],int p, float x)
 {
+  int i;
   float sum=0.0;
-  for(int i=k;i>=0;i--)
+  for(i=p;i>=0;i--)
   {
-   sum=sum*l+b[i];
+   sum=sum*x+a[i];
   } 
-  output(sum);
+  return sum;
 }
-void output(float o)
+void output(float result)
 {
-  printf("The sum of the polynomial is %f",o);
+  printf("The sum of the polynomial using horners method is %.2f",result);
 }
 
 int main()
 {
- input();
+ int p,x;
+ p=power();
+ x=value_of_x();
+ float a[p+1],sum;
+ coefficients(a,p);
+ sum=compute_poly(a,p,x);
+ output(sum);
  return 0;
 }
