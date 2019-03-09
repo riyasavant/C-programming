27687a96ac696dfd97c13301312523d4a6bdfb87
riyasavant <46701530+riyasavant@users.noreply.github.com>
2019-03-09 11:51:06 +0530

    Update binarysearch.c

--- lab/binarysearch.c
+++ lab/binarysearch.c
@@ -1,66 +1,86 @@
 #include<stdio.h>
-#include<stdlib.h>
-void input();
-void compute(int b[],int x,int y);
-void output(int x,int y);
-void input()
-{
-  int n,x;
-  printf("Enter the number of values in the array\n");
-  scanf("%d",&n);
-  int a[n];
-  printf("Enter the values in the array\n");
-  for(int i=0;i<n;i++)
-  {
+ int array_size()
+ {
+   int n;
+   printf("Enter the size of the array\n");
+   scanf("%d",&n);
+   return n;
+ }
+ void array(int a[],int n)
+ {
+   int i;
+   printf("Enetr the elements in the array\n");
+   for(i=0;i<n;i++)
+   {
     scanf("%d",&a[i]);
-  }
-  printf("Enter the number to be searched\n");
-  scanf("%d",&x);
-  compute(a,n,x);
-}
-void compute(int b[],int x,int y)
-{
-  int temp;
- for(int i=0;i<x;i++)
-  {
-    for(int j=i+1;j<x;j++)
+   }
+ }
+ int number()
+ {
+   int x;
+   printf("Enter the number to be searched\n");
+   scanf("%d",&x);
+   return x;
+ }
+ void sort(int a[],int n)
+ {
+   int i,j,temp;
+   for(i=0;i<n;i++)
+   {
+    for(j=i+1;j<n;j++)
+   {
+     if(a[i]>a[j])
      {
-       if(b[i]>b[j])
-       {
-        temp=b[i];
-        b[i]=b[j];
-        b[j]=temp;
-       }
+       temp=a[i];
+       a[i]=a[j];
+       a[j]=temp;
      }
-  }
-  int first=0,last=x-1;
-  int mid;
-  
+    }
+   }
+ }
+ int binary(int n,int a[],int x)
+ {
+   int first=0;
+   int last=n-1;
+   int mid;
     while(first<=last)
     {
      mid=(first+last)/2;
-     if(y>b[mid])
-     {
-      first=mid+1; 
-     }
-     else if(y<b[mid])
+     if(a[mid]>x)
      {
       last=mid-1;
      }
-     else
+     else if(a[mid]<x)
      {
-       output(mid,y);
-       exit(0);
+      first=mid+1;
      }
-    
-  }
-}
-void output(int x,int y)
-{
- printf("The value %d is found at the position %d in the array\n",y,x+1);
-}
-int main()
-{
-  input();
-  return 0;
-}
+     else if(a[mid]==x)
+     {  
+       return mid;
+     }
+    return -1;
+    }
+ }
+ void result(int pos)
+ {
+    if(pos==-1)
+    {
+      printf("The element is not present in the array");
+    }
+    else
+    {
+     printf("The element is present in the array at position %d\n",pos+1);
+    }
+ }
+ int main()
+ {
+   int n,x,pos;
+   n=array_size();
+   int a[n];
+   array(a,n);
+   x=number();
+   sort(a,n);
+   pos=binary(n,a,x);
+   result(pos);
+   return 0;
+ }
